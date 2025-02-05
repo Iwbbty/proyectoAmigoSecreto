12ce1bc12cdbc1c2bc31386ba2bbce38d4a7a92e
Iwbbty <claudio.portocarrero@unmsm.edu.pe>
2025-02-04 22:43:17 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let amigos = [];
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    if (amigos.includes(nombre)) {
+        alert("Este nombre ya fue agregado.");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach((nombre) => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay nombres en la lista para sortear.");
+        return;
+    }
+
+    let aleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[aleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>🎉 ¡El amigo secreto es: <strong>${amigoSorteado}</strong>! 🎁</li>`;
+}
